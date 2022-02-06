8f29eec95f4fc401073d50bce114bf79f1cbfb26
gyeonminchae <kmhae486@gmail.com>
2022-02-06 13:06:50 +0900

    Create React Project

--- management/src/App.js
+++ management/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customer = {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '채경민',
+  'birthday': '940224',
+  'gender': '남자',
+  'job': '직장인'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat = "logo" />
-        <h2>Develop management system!</h2>
-      </div>
+      <Customer
+        id={customer.id}
+        image={customer.image}
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
     );
   }
 }
